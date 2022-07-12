624669d8fabda098971bf5081eee18c53b7be2fc
Isa A. Sadiqli <isaas@code.local>
2022-07-12 12:21:00 +0400

    create classwork

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Slider> sliders = _context.Sliders.ToList();
+            return View(sliders);
+        }
+    }
+}
---
+++ WebApplication1/Models/Slider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+       public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public byte Order{ get; set; }
+    }
+}
